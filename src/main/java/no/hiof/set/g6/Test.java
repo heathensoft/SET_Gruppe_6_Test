929f7e59f1ff83c6763290cb00e83f7bbd67b06a
Frederik Dahl <frederikdahl13@gmail.com>
2024-09-15 14:23:56 +0200

    Test Project Gradle Structure

---
+++ src/main/java/no/hiof/set/g6/Test.java
@@ -0,0 +1,16 @@
+package no.hiof.set.g6;
+
+
+/**
+ * @author Frederik Dahl
+ * 15/09/2024
+ */
+
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Working");
+    }
+}
